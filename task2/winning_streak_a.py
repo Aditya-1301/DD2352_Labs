ce566a532cbee95ad007ce09b2c762d9e76ca723
Aditya Gupta <adigup@kth.se>
2024-01-31 22:46:01 +0100

    Added initial solution for winning_streak

--- task2/winning_streak_a.py
+++ task2/winning_streak_a.py
@@ -2,23 +2,29 @@ import sys
 
 n, p, k, map = None, None, None, {}
 
+
 def f(x, y):
     if y == 0: 
         return 1.0
     if x == 0 and y > 0:
         return 0.0
-    if f"{x-1}{k}" not in map:
-        map[f"{x-1}{k}"] = f(x-1, k)
-    if f"{x-1}{y-1}" not in map:
-        map[f"{x-1}{y-1}"] = f(x-1, y-1)
-    if x >= 1 and y >= 1:
-        return p * map[f"{x-1}{y-1}"] + (1 - p) * map[f"{x-1}{k}"]
+    if (x, y) in map:
+        return map[(x, y)]
+    result = p * f(x - 1, y - 1) + (1 - p) * f(x - 1, k)
+    map[(x, y)] = result
+    return result
+    # if f"{x-1}{k}" not in map:
+    #     map[f"{x-1}{k}"] = f(x-1, k)
+    # if f"{x-1}{y-1}" not in map:
+    #     map[f"{x-1}{y-1}"] = f(x-1, y-1)
+    # if x >= 1 and y >= 1:
+    #     return p * map[f"{x-1}{y-1}"] + (1 - p) * map[f"{x-1}{k}"]
 
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(10000)
+    sys.setrecursionlimit(100000)
     n = int(input())
     k = int(input())
     p = float(input())
-    print(f(n,k))
+    print(f(n, k))
     
